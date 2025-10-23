d4a764db7cb708a81a3b382c1f4c49562d2acdc5
Nguyen Hong Son <snguyenhong8@gmail.com>
2025-10-24 00:28:26 +0700

    ss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
-import GetSet from './GettersSetters/getterSetter.tsx';
+// import GetSet from './GettersSetters/getterSetter.tsx';
 import Inheritance from './Inheritance/Inheritance.tsx';
 
 
